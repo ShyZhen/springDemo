f04a1bf7a0076d603093490e48f6cc40bbb6121e
zhenhuaixiu <zhenhuaixiu@sznews.com>
2022-06-23 16:21:45 +0800

    1.找出修改共享变量的线程代码 2.选择一个共享实例作为锁lockObject 3.使用synchronized(lockObject) { ... }

--- src/main/java/com/fmock/demo/controller/ThreadController1.java
+++ src/main/java/com/fmock/demo/controller/ThreadController1.java
@@ -40,10 +40,17 @@ public class ThreadController1 {
 
     /**
      * Java程序使用synchronized关键字对一个对象进行加锁
+     * 通过加锁和解锁的操作，就能保证逻辑总是在一个线程执行期间，不会有其他线程进入此指令区间。就算被操作系统中断执行，其他线程也无法获得锁，导致无法进入指令区间。
+     * 只有执行线程释放锁后，其他线程才有机会获得锁并执行。
+     * synchronized保证了代码块在任意时刻最多只有一个线程能执行
+     *
+     * 我们来概括一下如何使用synchronized：
+     * 1.找出修改共享变量的线程代码
+     * 2.选择一个共享实例作为锁lockObject
+     * 3.使用synchronized(lockObject) { ... }
+     * 不用害怕synchronized中抛出异常，因为在结束处都会释放锁
      */
-    public static void func2() {
-
-    }
+    public static void func2() {}
 
 }
 
@@ -52,6 +59,14 @@ public class ThreadController1 {
 
 
 class Counter {
+    /**
+     * 用Counter.lock实例作为锁，两个线程在执行各自的synchronized(Counter.lock) { ... }代码块时，必须先获得锁，才能进入代码块进行。
+     * 执行结束后，在synchronized语句块结束会自动释放锁。这样一来，对Counter.count变量进行读写就不可能同时进行.
+     * 使用synchronized解决了多线程同步访问共享变量的正确性问题。但是，它的缺点是带来了性能下降。
+     * 因为synchronized代码块无法并发执行。此外，加锁和解锁需要消耗一定的时间，所以，synchronized会降低程序的执行效率。
+     */
+    public static final Object lock = new Object();
+
     public static int count = 0;  // volatile 该字段有局限性，不能用于依赖以前的值，此处为 `+=`,依赖之前的其他值
 }
 
@@ -59,7 +74,9 @@ class AddThread extends Thread {
     @Override
     public void  run() {
         for (int i =0; i<10000; i++) {
-            Counter.count += 1;
+            synchronized(Counter.lock) {  // 获取锁
+                Counter.count += 1;
+            }  // 释放锁
         }
     }
 }
@@ -68,7 +85,9 @@ class DecThread extends Thread {
     @Override
     public void run() {
         for (int i=0; i<10000; i++) {
-            Counter.count -= 1;
+            synchronized (Counter.lock) {
+                Counter.count -= 1;
+            }
         }
     }
 }
